cd5192dd7f354fccff3f30479eaefbad306d378c
frdrk00 <masterkooo@gmail.com>
2023-06-15 19:23:49 +0200

    email form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,9 +17,49 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
 
-  const handleSubmit = (e) => {};
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        'service_xe6qcyd',
+        'template_5mg6hzg',
+        {
+          from_name: form.name,
+          to_name: 'Frederik',
+          from_email: form.email,
+          to_email: 'masterkooo@gmail.com',
+          message: form.message,
+        },
+        'MSGAKvrAa-2O618HU'
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert('Thank you. I will get back to you as soon as possible.');
+
+          setForm({
+            name: '',
+            email: '',
+            message: '',
+          });
+        },
+        (error) => {
+          setLoading(false);
+
+          console.log(error);
+
+          alert('Something went wrong.');
+        }
+      );
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
